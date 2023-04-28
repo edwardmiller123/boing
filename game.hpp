22176313f42abedfb35546ec20b3eca77107d55a
edwardmiller123 <edwardomiller123@gmail.com>
2023-04-28 23:14:16 +0100

    Add level and player

--- game.hpp
+++ game.hpp
@@ -3,31 +3,56 @@
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
 
+#include "player.hpp"
+
 class Game
 {
 private:
-  sf::Window window;
+  sf::RenderWindow window;
   sf::Event event;
 
 public:
   bool gameRunning;
-  void initGame()
+  sf::Sprite level;
+  sf::Texture levelTexture;
+  Player player;
+
+  // Initialisation
+  int initGame()
   {
     gameRunning = true;
     window.create(sf::VideoMode(1600, 800), "Boing!");
+    initLevel();
   }
+
+  int initLevel() {
+    if (!levelTexture.loadFromFile("assets/level.png"))
+    {
+      std::cout << "Level texture didnt load";
+      return 0;
+    };
+    level.setTexture(levelTexture);
+    player.initPlayer({91, 150, 255, 243}, {479, 150, 255, 243}, {851, 150, 255, 243}, {1229, 150, 255, 243}, "assets/boingo.png");
+  }
+
+  // Game logic
   void run()
   {
     update();
     render();
   }
+  // Handles game logic.
   void update()
   {
     pollEvents();
   }
-
+  // Handles whats shown on screen.
   void render()
   {
+    window.clear();
+    window.draw(level);
+    window.draw(player.playerSprite);
+    window.display();
   }
 
   void pollEvents()
---
+++ player.hpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <SFML/Graphics.hpp>
+
+class Player
+{
+private:
+  sf::Texture playerTexture;
+  sf::Rect<int> frame1, frame2, frame3, frame4;
+  std::string texturePath;
+
+public:
+  sf::Sprite playerSprite;
+
+  int initPlayer(sf::Rect<int> newFrame1, sf::Rect<int> newFrame2, sf::Rect<int> newFrame3, sf::Rect<int> newFrame4, std::string newTexturePath)
+  {
+    frame1 = newFrame1;
+    frame2 = newFrame2;
+    frame3 = newFrame3;
+    frame4 = newFrame4;
+    texturePath = newTexturePath;
+    if (!playerTexture.loadFromFile(texturePath))
+    {
+      std::cout << "Player texture didn't load";
+      return 0;
+    };
+    playerSprite.setTexture(playerTexture);
+    // Sets starting frame
+    playerSprite.setTextureRect(frame1);
+    playerSprite.setScale(sf::Vector2f(0.125f, 0.125f));
+    playerSprite.setPosition(sf::Vector2f(10.f, 50.f));
+  }
+
+  void animate()
+  {
+  }
+
+  void jump()
+  {
+  }
+};
